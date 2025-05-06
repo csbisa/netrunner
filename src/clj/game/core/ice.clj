95bcf78ab1f51e4de03473b1ce8f0f44e3e70934
Clint Sbisa <csbisa@gmail.com>
2025-05-06 13:25:29 +0000

    Revert "[DISABLE-FOR-TEST] tweaks for subtype rendering"
    This reverts commit d71ff1bd78daea92c282fce7b325bb466e441358.

--- src/clj/game/core/ice.clj
+++ src/clj/game/core/ice.clj
@@ -18,13 +18,6 @@
     [clojure.math :refer [pow]]
     [medley.core :refer [find-first]]))
 
-(defn- tr-subtype [subtype]
-  (case subtype
-    "Barrier" "バリア"
-    "Code Gate" "コードゲート"
-    "Sentry" "セントリー"
-    subtype))
-
 ;; These should be in runs.clj, but `req` needs get-current-ice and
 ;; moving.clj needs set-current-ice
 (defn get-run-ices
@@ -545,8 +538,7 @@
                    (when-not (= #{"All"} subtypes)
                      (-> subtypes
                          (set/intersection (set (:subtypes ice)))
-                         (first)
-                         (tr-subtype))))]
+                         (first))))]
      {:type :break-subs :card (:title breaker) :ice (:title ice) :subtype subtype
       :subs (map :label (sort-by :index broken-subs))})))
 
